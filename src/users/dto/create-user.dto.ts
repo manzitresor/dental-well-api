4a753ea7f3a36066e90dfd47b7a92f5a34f20858
manzitresor <manzitre@gmail.com>
2025-08-12 15:33:56 +0200

    chore: implement create user endpoint

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsOptional, MinLength } from 'class-validator';
+import { UserRole } from 'src/util/role.enum';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  fullName: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+  phoneNumber: string;
+
+  @IsOptional()
+  roles?: UserRole;
+
+  @MinLength(8)
+  @IsNotEmpty()
+  password: string;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,8 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { UserRole } from 'src/util/role.enum';
 
 @Injectable()
 export class UsersService {
@@ -10,7 +12,13 @@ export class UsersService {
     @InjectRepository(User)
     private userRepository: Repository<User>,
   ) {}
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  async create(createUserDto: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    const user = this.userRepository.create({
+      ...createUserDto,
+      roles: createUserDto.roles || UserRole.PATIENT,
+      password: hashedPassword,
+    });
+    return this.userRepository.save(user);
   }
 }
