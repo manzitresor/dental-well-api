23dba01a4abab7120838e79cd71964db544d6c57
manzitresor <manzitre@gmail.com>
2025-08-12 17:25:45 +0200

    chore: add documentation of existing api

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,20 +1,42 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsOptional, MinLength } from 'class-validator';
 import { UserRole } from 'src/util/role.enum';
 
 export class CreateUserDto {
+  @ApiProperty({
+    example: 'Christelle Gihozo',
+    required: true,
+  })
   @IsNotEmpty()
   fullName: string;
 
+  @ApiProperty({
+    example: 'christelle@gmail.com',
+    required: true,
+  })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    example: '+250788123456',
+    required: true,
+  })
   @IsNotEmpty()
   phoneNumber: string;
 
+  @ApiProperty({
+    example: 'PATIENT',
+    required: false,
+    enum: UserRole,
+  })
   @IsOptional()
   roles?: UserRole;
 
+  @ApiProperty({
+    example: 'dental@123',
+    required: true,
+  })
   @MinLength(8)
   @IsNotEmpty()
   password: string;
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,22 +8,46 @@ import {
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a new User' })
+  @ApiResponse({
+    status: 201,
+    schema: {
+      example: {
+        id: 1,
+        name: 'John Doe',
+        email: 'johnDoe@gmail.com',
+        phoneNumber: '+250788123456',
+        roles: 'PATIENT',
+        createdAt: '2023-10-01T00:00:00.000',
+        updatedAt: '2023-10-01T00:00:00.000',
+      },
+    },
+  })
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
+  @ApiResponse({ status: 409, description: 'User already exists.' })
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Returns all registered User' })
+  @ApiResponse({ status: 200, description: 'Returns an array of users.' })
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Return a specific User' })
+  @ApiResponse({ status: 200, description: 'Returns a single user.' })
+  @ApiResponse({ status: 404, description: 'User not found.' })
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.findOne(id);
   }
